992b4bf6ecb979caa14722e8aa8844523588e02c
Paul Gregoire <mondain@gmail.com>
2024-06-28 10:28:13 -0700

    Update version to 2.0.1

--- io/src/main/java/org/red5/codec/ColorInfo.java
+++ io/src/main/java/org/red5/codec/ColorInfo.java
@@ -16,39 +16,39 @@ public class ColorInfo implements IExternalizable {
         colorConfig: {
           // number of bits used to record the color channels for each pixel
           bitDepth:                 number, // SHOULD be 8, 10 or 12
-      
+
           //
-          // colorPrimaries, transferCharacteristics and matrixCoefficients are defined 
-          // in ISO/IEC 23091-4/ITU-T H.273. The values are an index into 
-          // respective tables which are described in "Colour primaries", 
-          // "Transfer characteristics" and "Matrix coefficients" sections. 
+          // colorPrimaries, transferCharacteristics and matrixCoefficients are defined
+          // in ISO/IEC 23091-4/ITU-T H.273. The values are an index into
+          // respective tables which are described in "Colour primaries",
+          // "Transfer characteristics" and "Matrix coefficients" sections.
           // It is RECOMMENDED to provide these values.
           //
-      
+
           // indicates the chromaticity coordinates of the source color primaries
           colorPrimaries:           number, // enumeration [0-255]
-      
+
           // opto-electronic transfer characteristic function (ex. PQ, HLG)
           transferCharacteristics:  number, // enumeration [0-255]
-      
+
           // matrix coefficients used in deriving luma and chroma signals
           matrixCoefficients:       number, // enumeration [0-255]
         },
-      
+
         hdrCll: {
           //
           // maximum value of the frame average light level
           // (in 1 cd/m2) of the entire playback sequence
           //
           maxFall:  number,     // [0.0001-10000]
-      
+
           //
           // maximum light level of any single pixel (in 1 cd/m2)
           // of the entire playback sequence
           //
           maxCLL:   number,     // [0.0001-10000]
         },
-      
+
         //
         // The hdrMdcv object defines mastering display (i.e., where
         // creative work is done during the mastering process) color volume (a.k.a., mdcv)
@@ -65,7 +65,7 @@ public class ColorInfo implements IExternalizable {
           // 1931 color space.
           //
           // Values SHALL be specified with four decimal places. The x coordinate SHALL
-          // be in the range [0.0001, 0.7400]. The y coordinate SHALL be 
+          // be in the range [0.0001, 0.7400]. The y coordinate SHALL be
           // in the range [0.0001, 0.8400].
           //
           redX:         number,
@@ -76,13 +76,13 @@ public class ColorInfo implements IExternalizable {
           blueY:        number,
           whitePointX:  number,
           whitePointY:  number,
-      
+
           //
           // max/min display luminance of the mastering display (in 1 cd/m2 ie. nits)
           //
           // note: ST 2086:2018 - SMPTE Standard specifies minimum display mastering
           // luminance in multiples of 0.0001 cd/m2.
-          // 
+          //
           // For consistency we specify all values
           // in 1 cd/m2. Given that a hypothetical perfect screen has a peak brightness
           // of 10,000 nits and a black level of .0005 nits we do not need to
@@ -148,28 +148,26 @@ public class ColorInfo implements IExternalizable {
     }
 
     public class ColorConfig implements Serializable {
-        
+
         // number of bits used to record the color channels for each pixel
         Number bitDepth = 8; // SHOULD be 8, 10 or 12
- 
+
         // colorPrimaries, transferCharacteristics and matrixCoefficients are defined in ISO/IEC 23091-4/ITU-T H.273.
         // The values are an index into respective tables which are described in "Colour primaries", "Transfer
         // characteristics" and "Matrix coefficients" sections. It is RECOMMENDED to provide these values.
-        
+
         // indicates the chromaticity coordinates of the source color primaries
         Number colorPrimaries; // enumeration [0-255]
-        
+
         // opto-electronic transfer characteristic function (ex. PQ, HLG)
         Number transferCharacteristics; // enumeration [0-255]
-      
+
         // matrix coefficients used in deriving luma and chroma signals
         Number matrixCoefficients; // enumeration [0-255]
 
         @Override
         public String toString() {
-            return "ColorConfig [bitDepth=" + bitDepth + ", colorPrimaries=" + colorPrimaries
-                    + ", transferCharacteristics=" + transferCharacteristics + ", matrixCoefficients="
-                    + matrixCoefficients + "]";
+            return "ColorConfig [bitDepth=" + bitDepth + ", colorPrimaries=" + colorPrimaries + ", transferCharacteristics=" + transferCharacteristics + ", matrixCoefficients=" + matrixCoefficients + "]";
         }
 
     }
@@ -178,7 +176,7 @@ public class ColorInfo implements IExternalizable {
 
         // maximum value of the frame average light level (in 1 cd/m2) of the entire playback sequence
         Number maxFall; // [0.0001-10000]
-    
+
         // maximum light level of any single pixel (in 1 cd/m2) of the entire playback sequence
         Number maxCLL; // [0.0001-10000]
 
@@ -195,32 +193,23 @@ public class ColorInfo implements IExternalizable {
 
         // Values SHALL be specified with four decimal places. The x coordinate SHALL be in the range [0.0001, 0.7400].
         // The y coordinate SHALL be in the range [0.0001, 0.8400].
-        Number redX,
-        redY,
-        greenX,
-        greenY,
-        blueX,
-        blueY,
-        whitePointX,
-        whitePointY;
-    
+        Number redX, redY, greenX, greenY, blueX, blueY, whitePointX, whitePointY;
+
         // max/min display luminance of the mastering display (in 1 cd/m2 ie. nits)
         // note: ST 2086:2018 - SMPTE Standard specifies minimum display mastering luminance in multiples of 0.0001
         // cd/m2.
-        // 
+        //
         // For consistency we specify all values in 1 cd/m2. Given that a hypothetical perfect screen has a peak
         // brightness of 10,000 nits and a black level of .0005 nits we do not need to switch units to 0.0001 cd/m2
         // to increase resolution on the lower end of the minLuminance property. The ranges (in nits) mentioned
         // below suffice the theoretical limit for Mastering Reference Displays and adhere to the SMPTE ST 2084
         // standard (a.k.a., PQ) which is capable of representing full gamut of luminance level.
         Number maxLuminance, // [5-10000]
-        minLuminance; // [0.0001-5]
+                minLuminance; // [0.0001-5]
 
         @Override
         public String toString() {
-            return "HdrMdcv [redX=" + redX + ", redY=" + redY + ", greenX=" + greenX + ", greenY=" + greenY + ", blueX="
-                    + blueX + ", blueY=" + blueY + ", whitePointX=" + whitePointX + ", whitePointY=" + whitePointY
-                    + ", maxLuminance=" + maxLuminance + ", minLuminance=" + minLuminance + "]";
+            return "HdrMdcv [redX=" + redX + ", redY=" + redY + ", greenX=" + greenX + ", greenY=" + greenY + ", blueX=" + blueX + ", blueY=" + blueY + ", whitePointX=" + whitePointX + ", whitePointY=" + whitePointY + ", maxLuminance=" + maxLuminance + ", minLuminance=" + minLuminance + "]";
         }
 
     }
