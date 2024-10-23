97d2c6c2c65381bd0e8b6d31a0e7e9f1f08bb58b
Paul Gregoire <mondain@gmail.com>
2024-10-23 08:37:10 -0700

    Bump version to 2.0.7

--- common/src/main/java/org/red5/server/api/Red5.java
+++ common/src/main/java/org/red5/server/api/Red5.java
@@ -57,12 +57,12 @@ public final class Red5 {
     /**
      * Server version with revision
      */
-    public static final String VERSION = "Red5 Server 2.0.6";
+    public static final String VERSION = "Red5 Server 2.0.7";
 
     /**
      * Server version for fmsVer requests
      */
-    public static final String FMS_VERSION = "RED5/2,0,6,0";
+    public static final String FMS_VERSION = "RED5/2,0,7,0";
 
     /**
      * Server capabilities
