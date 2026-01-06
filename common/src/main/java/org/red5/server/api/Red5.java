a1700e6a033feb2df1b4323a825accf1bfd9bcb7
Paul Gregoire <mondain@gmail.com>
2026-01-06 07:00:00 -0800

    Update version to 2.0.27

--- common/src/main/java/org/red5/server/api/Red5.java
+++ common/src/main/java/org/red5/server/api/Red5.java
@@ -57,12 +57,12 @@ public final class Red5 {
     /**
      * Server version with revision
      */
-    public static final String VERSION = "Red5 Server 2.0.26";
+    public static final String VERSION = "Red5 Server 2.0.27";
 
     /**
      * Server version for fmsVer requests
      */
-    public static final String FMS_VERSION = "RED5/2,0,26,0";
+    public static final String FMS_VERSION = "RED5/2,0,27,0";
 
     /**
      * Server capabilities
